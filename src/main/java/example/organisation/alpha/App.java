faf68878ea9df6752fff9d6c22cf5b56bd07f682
Helmut Frohner <helmut.frohner@allianz.at>
2018-06-06 16:27:26 +0200

    Initial add.

---
+++ src/main/java/example/organisation/alpha/App.java
@@ -0,0 +1,13 @@
+package example.organisation.alpha;
+
+/**
+ * Hello Alpha!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Alpha!" );
+    }
+}
