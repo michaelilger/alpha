5bfa21364fc5befea58fe3c6e92d6f8dee8d45df
Helmut Frohner <helmut.frohner@allianz.at>
2018-06-14 17:32:10 +0200

    was anderes

--- src/main/java/example/organisation/alpha/App.java
+++ src/main/java/example/organisation/alpha/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Alpha!" );
-        System.out.println( "Irgendwas!" );
+        System.out.println( "Irgendwas anderes!" );
     }
 }
